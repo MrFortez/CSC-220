e125a5c91142878f6ca867af0f8b7ec0bd1f099a
Brandon Fortes <55511864+MrFortez@users.noreply.github.com>
2024-10-13 20:22:14 -0500

    A lot of progress on Classroom List

--- Classroom List/src/Group.java
+++ Classroom List/src/Group.java
@@ -1,31 +1,110 @@
 
 
 public class Group {
+    // The array storing the students in this group
     private Student[] members;
-    private static int size = 2;
+
+    // The number of students in the group. The same as doing members.length but not including null items.
+    private int numOfStudents = 0;
+
+    // A static variable storing the target size for groups. 
+    // Note that if the total number of students isnt evenly divisable by
+    // the size, then this progam will place the remaining students into their 
+    // own group, with the empty slots of the group being set to null.
+    // 2 is the default, but it can be changed by the user.
+    private static int targetGroupSize = 2;
 
     public Group() {
-        members = new Student[size];
+        members = new Student[targetGroupSize];
     }
 
-    public static void setSize(int size) {
-        Group.size = size;
+    public static void setTargetGroupSize(int size) {
+        Group.targetGroupSize = size;
     }
 
-    public double getGroupScore() {
-        int score = 0;
-        for (int i = 0; i <)
-                score += studentA.getCompat
+    public static int getTargetGroupSize() {
+        return targetGroupSize;
+    }
+
+    // Takes in any number of student objects, and takes those student
+    // objects and adds them into any non occupied spots in this group.
+    // If the group beoomes full, any remaining students will not be added.
+    public void populateGroup(Student... students) {
+
+        // i is the group index, and j is the index for the array of students passed into this function.
+        // The loop will stop when either of these indexes surpass the limit of their respective array.
+        for (int i = 0, j = 0; (i < members.length) && (j < students.length); i++) {
+            if (members[i] == null) {
+                members[i] = students[j];
+                j++;
+
+                numOfStudents++;
             }
         }
-        return 0;
+    }
+
+    // Calculates the sum of the compatability between each student by the group, 
+    // then divides it by the number of students in the group.
+    public double getGroupScore() {
+        int score = 0;
+        
+        // Loop through the members of this group.
+        for (int i = 0; i < members.length; i++) {
+
+            // Another loop that is offset by the outer loop by +1. The way these 
+            // indexes are updated, these for loops will check the compatability of
+            // every possible pair of the students in this group exactly once.
+            for (int j = i + 1; j < getNumberOfStudents(); j++) {
+                score += members[i].getCompatabilityScore(members[j]);
+            }
+        }
+                
+        return score / (double) getNumberOfStudents();
+    }
+
+    // Get the number of students in the group. This is different from
+    // members.length in the case where the group is not full.
+    // Note that the way students are added to groups, all null(empty) slots
+    // will be at the back of the array. 
+    public int getNumberOfStudents() {
+        // int count = 0;
+        // for (Student member : members) {
+        //     if (member == null) {
+        //         break;
+        //     }
+        //     count++;
+        // }
+
+        // return count;
+        return numOfStudents;
+    }
+
+    // Returns the student object at the given index
+    public Student getStudent(int index) {
+        try {
+            return members[index];
+        }
+        catch (Exception e) {
+            System.err.println("Invalid Index; in getStudent()");
+            return null;
+        }
+    }
+
+    // Replaces the student at the given index with the given student.
+    public void replaceStudent(int index, Student newStudent) {
+        try {
+            members[index] = newStudent;
+        }
+        catch (Exception e) {
+            System.err.println("Invalid Index; in replaceStudent()");
+        }
     }
 
     public String toString() {
         String result = "Members: [";
-        for (int i = 0; i < members.length; i++) {
+        for (int i = 0; i < getNumberOfStudents(); i++) {
             result += members[i].getName();
-            result += (i == (members.length - 1)) ? "] " : ", ";
+            result += (i == (getNumberOfStudents() - 1)) ? "] " : ", ";
         }
 
         result += "Group Score: " + getGroupScore();
--- Classroom List/src/GroupTest.java
+++ Classroom List/src/GroupTest.java
@@ -1,60 +1,48 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Scanner;
+import java.util.Random;
 
-public class GroupTest {
+class GroupTest {
     public static void main(String[] args) {
 
         // Generate the array of Students.
         Student[] students = generateStudentList();
 
+         // Prints out each student's name, list number, and preference rankings. Used for testing purposes.
+         for (Student student : students) {
+            System.out.println(student);
+        }
+        
         // Print out the preference scores, used for testing.
-        for (Student student : students) {
-            String result = "[";
-            for (int i = 0; i < student.getPreferences().length; i++) {
-                result += student.getPreferences()[i];
-                result += (i == (student.getPreferences().length - 1)) ? "] " : ", ";
-            }
-            System.out.println(result);
+        // for (Student student : students) {
+        //     String result = "[";
+        //     for (int i = 0; i < student.getPreferences().length; i++) {
+        //         result += student.getPreferences()[i];
+        //         result += (i == (student.getPreferences().length - 1)) ? "] " : ", ";
+        //     }
+        //     System.out.println(result);
+        // }
+
+        // have the user input the group size
+        userInputedGroupSize(students);
+
+        // Generate random groups
+        Group[] groups = generateRandomGroups(students);
+       
+        for (Group group : groups) {
+            System.out.println(group);
         }
 
-        // Create a Scanner for user input.
-        Scanner inputScanner = new Scanner(System.in);
-
-        // String input is used to store the user's input.
-        String input = "";
-
-        // boolean validSize is used to loop the input prompt until the user inputs a valid group size.
-        boolean validSize = false;
-
-        // Loop until the user inputs a valid group size.
-        while (!validSize) {
-            System.out.print("How many students will be in each group?  ");
-
-            // Get the user's input
-            input = inputScanner.nextLine();
-
-            // The try is here incase the user inputs anything other than an integer.
-            try {
-
-                // This checks whether the user's inputed number is a valid size. 
-                // This is defined as being between 2 Students and half the total number of students rounded up.
-                // Any other group size is nonsensical in the context of a classroom.
-                // We round up in case the number of students is odd and the teacher wants to split the group in half.
-                if ((Integer.parseInt(input)) >= 2 && (Integer.parseInt(input) <= Math.ceilDiv(students.length, 2))) {
-                    validSize = true;
-                }
-                else {
-                    System.out.println("The group size must be between 2 and " + Math.ceilDiv(students.length, 2));
-                }
-            }
-            catch (Exception e){
-                System.err.println("The size has to be an integer.");
-            }
+        for (int i = 0; i < 100000; i++) {
+            randomSwap(groups);
         }
-        Group.setSize(Integer.parseInt(input));
 
+        System.out.println();
 
+        for (Group group : groups) {
+            System.out.println(group);
+        }
 
 
 
@@ -108,11 +96,6 @@ public class GroupTest {
             // Close the buffered reader because I no longer need its services.
             br.close();
 
-            // Prints out each student's name, list number, and preference rankings. Used for testing purposes.
-            for (Student student : students) {
-                System.out.println(student);
-            }
-
             return students;
         
 
@@ -123,4 +106,165 @@ public class GroupTest {
         return null;
     }
 
+    // Sets the group size via the user's input.
+    public static void userInputedGroupSize(Student[] students) {
+        // Create a Scanner for user input.
+        Scanner inputScanner = new Scanner(System.in);
+
+        // String input is used to store the user's input.
+        String input = "";
+
+        // boolean validSize is used to loop the input prompt until the user inputs a valid group size.
+        boolean validSize = false;
+
+        // Loop until the user inputs a valid group size.
+        while (!validSize) {
+            System.out.print("How many students will be in each group?  ");
+
+            // Get the user's input
+            input = inputScanner.nextLine();
+
+            // The try is here incase the user inputs anything other than an integer.
+            try {
+
+                // This checks whether the user's inputed number is a valid size. 
+                // This is defined as being between 2 Students and half the total number of students rounded up.
+                // Any other group size is nonsensical in the context of a classroom.
+                // We round up in case the number of students is odd and the teacher wants to split the group in half.
+                if ((Integer.parseInt(input)) >= 2 && (Integer.parseInt(input) <= Math.ceilDiv(students.length, 2))) {
+                    validSize = true;
+                }
+                else {
+                    System.out.println("The group size must be between 2 and " + Math.ceilDiv(students.length, 2));
+                }
+            }
+            catch (Exception e){
+                System.err.println("The size has to be an integer.");
+            }
+
+            
+        }
+        Group.setTargetGroupSize(Integer.parseInt(input));
+        inputScanner.close();
+
+    }
+
+    // Generates N/s groups (where N is the number of students and s is the size of each group, with the remainder being its own group)
+    // Each group will consist of a random assortment of students from the given list.
+    public static Group[] generateRandomGroups(Student[] students) {
+        Student[] randomStudents = shuffle(students);
+
+        // Declare and Initialize an array of ceil(N/s) groups. 
+        Group[] groups = new Group[Math.ceilDiv(students.length, Group.getTargetGroupSize())];
+
+        // Used to keep track of our position in the randomStudents array.
+        int index = 0;
+
+        // Loop through and initialize each group in the groups list.
+        for (int i = 0; i < groups.length; i++) {
+            groups[i] = new Group();
+
+            // For each group, we need to fill each of it's slots with the next 
+            // student in the randomStudents list.
+            for (int j = 0; j < Group.getTargetGroupSize(); j++, index++) {
+
+                // breaking the loop when the final group has more slots but there
+                // are no students remaining, so as to not cause a ListOutOfBounds exception.
+                if (index >= randomStudents.length) {
+                    break;
+                }
+                
+                // Add the student into the group.
+                groups[i].populateGroup(randomStudents[index]);
+            }
+
+
+        }
+
+        return groups;
+    }
+
+    // randomly chooses a student from two different groups. If swapping the 
+    // chosen students would increase the combined compatability score,
+    // this function will swap them.
+    public static void randomSwap(Group[] groups) {
+        
+        // Makes an object of the random class.
+        Random random = new Random();
+
+        // temp variables used to keep track of which two groups we are swapping between
+        Group targetGroupA;
+        Group targetGroupB;
+
+        // The indexes of the students that we are swapping.
+        int targetIndexA;
+        int targetIndexB;
+
+        double originalScore;
+        double newScore;
+
+        // Target a random group from the given groups array.
+        targetGroupA = groups[random.nextInt(groups.length)];
+
+        // Ensure that the target groups are different
+        do {
+            targetGroupB = groups[random.nextInt(groups.length)];
+        }
+        while (targetGroupB.equals(targetGroupA));
+       
+        // Randomly decide which student in each group will be swapped.
+        targetIndexA = random.nextInt(targetGroupA.getNumberOfStudents());
+        targetIndexB = random.nextInt(targetGroupB.getNumberOfStudents());
+
+        // Store the original score prior to swapping.
+        originalScore = targetGroupA.getGroupScore() + targetGroupB.getGroupScore();
+
+        // Swap.
+        swapBetweenGroups(targetGroupA, targetGroupB, targetIndexA, targetIndexB);
+
+        // Store the new score post swapping.
+        newScore = targetGroupA.getGroupScore() + targetGroupB.getGroupScore();
+
+        // If the original score was higher, undo the swap.
+        if (originalScore > newScore) {
+            swapBetweenGroups(targetGroupA, targetGroupB, targetIndexA, targetIndexB);
+        }
+
+
+    }
+
+    // Given two Groups, swap the student at indexA with the student at indexB
+    public static void swapBetweenGroups(Group groupA, Group groupB, int indexA, int indexB) {
+        Student temp = groupA.getStudent(indexA);
+        groupA.replaceStudent(indexA, groupB.getStudent(indexB));
+        groupB.replaceStudent(indexB, temp);
+    }
+
+
+    // Helper function, returns the given array with the order of its items randomized.
+    public static <T> T[] shuffle(T[] array) {
+        // Makes an object of the random class.
+        Random random = new Random();
+
+        // Loop through the array, such that each item will be swapped at least once.
+        for (int i = 0; i < array.length; i++) {
+
+            // Generate the position that the item at index i will be swapped with.
+            int swapIndex = random.nextInt(array.length - 1);
+
+            // Swap.
+            T temp = array[i];
+            array[i] = array[swapIndex];
+            array[swapIndex] = temp;
+
+        }
+
+        return array;
+    }
+
+  
 }
+
+
+
+
